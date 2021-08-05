8cc866c3c51e806c200940371e807cda32589c6f
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2021-08-05 10:27:30 +0200

    Add files via upload

---
+++ reverse-str.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+// for (var reversedStr = "", i = str.length - 1; i >= 0; i--)
+//{reversedStr += str[i];}
+//return reversedStr;
+console.log(reverseString('hello'));
